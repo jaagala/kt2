a09233a5de64a5249252702179b345a33e9ef3c3
Student Admin <jaland@tlu.ee>
2019-04-29 10:50:01 +0300

    Interface

---
+++ kontrolltoo/src/main/java/kontrolltoo/AnalyticsInterface.java
@@ -0,0 +1,8 @@
+package main.java.kontrolltoo;
+
+interface AnalyticsInterface{
+    public void read_my_file();
+    public void split_words_into_pieces();
+    public void turn_words_to_lowercase();
+    public void count_words();
+}
